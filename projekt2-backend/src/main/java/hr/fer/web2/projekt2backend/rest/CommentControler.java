1d2fcac5760e305e088aab8f8a89c6a1c5123884
MarkoTunjic <marko.tunjic21@gmail.com>
2022-10-31 01:49:37 +0100

    Add controller implementations

--- projekt2-backend/src/main/java/hr/fer/web2/projekt2backend/rest/CommentControler.java
+++ projekt2-backend/src/main/java/hr/fer/web2/projekt2backend/rest/CommentControler.java
@@ -1,10 +1,35 @@
 package hr.fer.web2.projekt2backend.rest;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hr.fer.web2.projekt2backend.models.dto.CommentDTO;
+import hr.fer.web2.projekt2backend.service.CommentService;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentControler {
+    private final CommentService commentService;
 
+    public CommentControler(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createNewComment(@RequestBody String commentText) {
+        commentService.createNewComment(commentText);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentDTO>> getAllComments() {
+        return ResponseEntity.ok(commentService.getAllComments());
+    }
 }
--- projekt2-backend/src/main/java/hr/fer/web2/projekt2backend/rest/PrincipalController.java
+++ projekt2-backend/src/main/java/hr/fer/web2/projekt2backend/rest/PrincipalController.java
@@ -1,10 +1,35 @@
 package hr.fer.web2.projekt2backend.rest;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hr.fer.web2.projekt2backend.models.dto.PrincipalDTO;
+import hr.fer.web2.projekt2backend.service.PrincipalService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/admin/principals")
 public class PrincipalController {
+    private final PrincipalService principalService;
 
+    public PrincipalController(PrincipalService principalService) {
+        this.principalService = principalService;
+    }
+
+    @GetMapping("/insecure")
+    ResponseEntity<List<PrincipalDTO>> getAllPrincipalsInsecure() {
+        return ResponseEntity.ok(principalService.getAllPrincipals());
+    }
+
+    @GetMapping("/secure")
+    @SecurityRequirement(name = "BearerAuthentication")
+    @PreAuthorize("!hasRole('')")
+    ResponseEntity<List<PrincipalDTO>> getAllPrincipalsSecure() {
+        return ResponseEntity.ok(principalService.getAllPrincipals());
+    }
 }
